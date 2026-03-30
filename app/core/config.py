fa64d5a187a38714aba7c64add7a7aa50d756a38
Lakshya <lakshya19j@gmail.com>
2026-03-31 02:07:58 +0530

    database

--- app/core/config.py
+++ app/core/config.py
@@ -5,5 +5,7 @@ load_dotenv()
 class Settings:
     APP_NAME: str = os.getenv("APP_NAME", "FastAPI App")
     DEBUG: bool = os.getenv("DEBUG", "False") == "True"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+    DATABASE_URL_SYNC: str = os.getenv("DATABASE_URL_SYNC", "")
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import time
 from fastapi import FastAPI, Request
 from app.api.v1.router import router
-from core.config import settings
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.APP_NAME,
     debug=settings.DEBUG
 )
-
+app.include_router(router, prefix="/api/v1")
 @app.middleware("http")
 async def log_requests(request:Request, call_next):
     start_time = time.time()
@@ -16,10 +16,10 @@ async def log_requests(request:Request, call_next):
 
     process_time = time.time()-start_time
     print(
-        f"{request.method} {request.url.path}"
-        f"completed in {process_time:.4f}s"
-        f"status={response.status_code}"
+        f"{request.method} {request.url.path}\n"
+        f"completed in {process_time:.4f}s\n"
+        f"status={response.status_code}\n"
     )
     return response
 
-app.include_router(router, prefix="/api/v1")
+
